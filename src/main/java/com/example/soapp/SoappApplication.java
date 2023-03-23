3a6c1c9c0d4b1b09d59ba54dced3e0519dd5e9c5
marion <marion.chineaud@gmail.com>
2023-03-23 16:51:17 +0100

    first commit

---
+++ src/main/java/com/example/soapp/SoappApplication.java
@@ -0,0 +1,13 @@
+package com.example.soapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoappApplication.class, args);
+    }
+
+}
